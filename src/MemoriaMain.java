e6be9ae28733d1d969f79b849f0e7f86a11fcbcb
BENNIDRUMS <paulobeniciojr@gmail.com>
2026-09-05 03:46:15 -0300

    Atividade memoria equals e hashCode

--- src/MemoriaMain.java
+++ src/MemoriaMain.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
+import java.util.HashSet;
 public class MemoriaMain {
+
+    public static void main(String[] args) {
+
+        HashSet<Pessoa> pessoas = new HashSet<>();
+
+        pessoas.add(new Pessoa("João", 25));
+        pessoas.add(new Pessoa("João", 25));
+        pessoas.add(new Pessoa("Maria", 30));
+        pessoas.add(new Pessoa("Pedro", 25));
+
+        for (Pessoa pessoa : pessoas) {
+            System.out.println("\nNome: " + pessoa.getNomePessoa());
+            System.out.println("Idade: " + pessoa.getIdadePessoa());
+        }
+        System.out.print("\nApenas " + pessoas.size()
+                + " pessoas ficaram no total, pois duas pessoas possuíam o mesmo nome e a mesma idade. "
+                + "Por isso, foram consideradas iguais pelo HashSet.\n");
+    }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class Pessoa {
+
+    private String nomePessoa;
+    private int idadePessoa;
+
+    public Pessoa(String nomePessoa, int idadePessoa) {
+        this.nomePessoa = nomePessoa;
+        this.idadePessoa = idadePessoa;
+    }
+    public String getNomePessoa() {
+        return nomePessoa;
+    }
+    public int getIdadePessoa() {
+        return idadePessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return idadePessoa == pessoa.idadePessoa && Objects.equals(nomePessoa, pessoa.nomePessoa);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nomePessoa, idadePessoa);
+    }
 }
